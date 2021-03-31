e3c382e02e3bf80e3130ec09c01858a27a5288e7
dayousseful <77815660+dayousseful@users.noreply.github.com>
2021-03-31 17:41:20 -0400

    Add files via upload

---
+++ week10/components/FavFood.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {StyleSheet, Text, View } from 'react-native';
+
+export default class FavFood extends Component {
+    render(){
+        return(
+          <view>
+            <text> My Fav Food is Pizza</text>
+          </view>
+        );
+    }
+}
---
+++ week10/components/Name.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {StyleSheet, Text, View } from 'react-native';
+
+export default class Name extends Component {
+    render(){
+        return(
+          <view>
+            <text> Youssef Ibrahim</text>
+          </view>
+        );
+    }
+}
